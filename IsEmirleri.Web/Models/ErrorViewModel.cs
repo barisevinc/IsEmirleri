51652ef4167bfec7b44214e1ac9b0e510398cf55
Emre Ozcelikel <emre@ozcelikel.com>
2024-07-04 17:00:11 +0300

    Add project files.

---
+++ IsEmirleri.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IsEmirleri.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
