8b9db5e81d19b53c87c3b0ad21df8ce4ea61f9be
Emre Güler <guler.emre.99@gmail.com>
2024-07-05 18:05:05 +0300

    Generic servis yazıldı. Program.cs ayarları yapıldı. Business extansion yazıldı.

---
+++ IsEmirleri.Business/BusinessExtansion.cs
@@ -0,0 +1,25 @@
+﻿using IsEmirleri.Business.Shared.Abstract;
+using IsEmirleri.Business.Shared.Concrete;
+using IsEmirleri.Repository.Shared.Abstract;
+using IsEmirleri.Repository.Shared.Concrete;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsEmirleri.Business
+{
+    public static class BusinessExtansion
+    {
+        public static void AddBusinessDI(this IServiceCollection services)
+        {
+            services.AddScoped(typeof (IService<> ), typeof (Service<>));
+        }
+        public static void AddRepositoryDI(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+        }
+    }
+}
--- IsEmirleri.Web/Program.cs
+++ IsEmirleri.Web/Program.cs
@@ -1,12 +1,14 @@
 using IsEmirleri.Data;
 using Microsoft.EntityFrameworkCore;
+using IsEmirleri.Business;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddBusinessDI();
+builder.Services.AddRepositoryDI();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
