0fc0f0b3f5a6b6497f640cf93f2b42f1c7200686
Emre Güler <guler.emre.99@gmail.com>
2024-07-12 14:52:26 +0300

    Projenin son sürümüne göre uyarlandı.

---
+++ IsEmirleri.DTO/CustomerDTOs/CustomerGetAllDto.cs
@@ -0,0 +1,14 @@
+﻿using IsEmirleri.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsEmirleri.DTO.CustomerDTOs
+{
+    public class CustomerGetAllDto : Customer
+    {
+        public int UserCount { get; set; }
+    }
+}
