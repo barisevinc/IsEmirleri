52667f66486d4d45de837607d825c88dc99c2a4d
hafsaaulusall <54844226+hafsaaulusall@users.noreply.github.com>
2024-07-15 23:10:10 +0300

    Proje sayfası yapıldı

---
+++ IsEmirleri.DTO/ProjectDTOs/ProjectAddDto.cs
@@ -0,0 +1,16 @@
+﻿using IsEmirleri.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsEmirleri.DTO.ProjectDTOs
+{
+    public class ProjectAddDto:Project
+    {
+        public Project Project { get; set; }
+
+        public List<string> UserEmails { get; set; }
+    }
+}
