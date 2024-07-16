2522f0ad13335637ad0e7da96647533389f7e0bc
sena aydın <btlsnaydin@gmail.com>
2024-07-16 23:15:51 +0300

    öncelik sayfası hazırlandı

--- IsEmirleri.Business/Abstract/IPriorityService.cs
+++ IsEmirleri.Business/Abstract/IPriorityService.cs
@@ -1,8 +1,11 @@
 ﻿using IsEmirleri.Business.Shared.Abstract;
+using IsEmirleri.DTO.CustomerDTOs;
+using IsEmirleri.DTO.PriorityDTOs;
 using IsEmirleri.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,5 +13,11 @@ namespace IsEmirleri.Business.Abstract
 {
     public interface IPriorityService:IService<Priority>
     {
+        IQueryable<PriorityGetAllDto> GetAllWithTaskCount();
+        IQueryable<Priority> GetAll(Expression<Func<Priority, bool>>predicate);
+        Priority GetById(int id);
+        Priority Add (Priority priority);
+        Priority UpdatePriority (Priority priority);
+        bool Delete (int id);
     }
 }
