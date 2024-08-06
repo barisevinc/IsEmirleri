bd2a2e8bffec6642d4169d1216e290e5195267c2
Burak Yasin Uyanık <burakyasin3370@gmail.com>
2024-08-06 22:28:24 +0300

    ok

---
+++ IsEmirleri.Api/Controllers/MissionController.cs
@@ -0,0 +1,25 @@
+﻿using IsEmirleri.Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IsEmirleri.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MissionController : ControllerBase
+    {
+        private readonly IMissionService missionService;
+
+        public MissionController(IMissionService missionService)
+        {
+            this.missionService = missionService;
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetAll([FromRoute(Name="id")] int id)
+        {
+
+            return Ok(missionService.GetAll());
+        }
+    }
+}
